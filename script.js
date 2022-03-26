cdba1f5c90c051e4f504d66678e23a6d6e7bedef
Shobuj <shobuj.mas@gmail.com>
2022-03-26 13:38:45 +0600

    footer clicked funtionality added

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const ul = document.querySelector('ul');
-const lists = ul.querySelectorAll('li');
+const ul = document.querySelector("ul");
+const lists = ul.querySelectorAll("li");
+const footerDivs = document.querySelectorAll(".footer div");
 
 lists.forEach(li => {
    li.onclick = function() {
@@ -10,5 +11,15 @@ lists.forEach(li => {
 
       this.classList.add('selected');
       this.insertAdjacentHTML('afterbegin', "<span></span>");
+   }
+})
+
+footerDivs.forEach(div => {
+   div.onclick = function() {
+      // remove class from all divs.
+      footerDivs.forEach(i => i.classList.remove('footer-div-select'));
+
+      // add class to clicked div.
+      this.classList.add('footer-div-select');
    }
 })
